1be791523f8a922e395c11758dd4f9594aaadc81
sunil <sunil.crud@gmail.com>
2026-05-01 21:32:29 +0530

    update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { styled } from "nativewind";
 import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
+
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
