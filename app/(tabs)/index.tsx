2a6560426393a745e1f70217f16acea9eb6168ed
sunil <sunil.crud@gmail.com>
2026-05-01 21:37:12 +0530

    add blank line for matting

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,6 @@ import { styled } from "nativewind";
 import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
-
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
