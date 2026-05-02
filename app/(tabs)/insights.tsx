65b06e72e9331315192015c3f9cd86e508c3a95d
sunil <sunil.crud@gmail.com>
2026-05-02 14:50:03 +0530

    impliment home component

--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -2,6 +2,7 @@ import { styled } from "nativewind";
 import React from 'react';
 import { Text } from 'react-native';
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Insights() {
